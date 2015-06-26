64b1cd0adc709225a79cec0dc22d61e03882dbbf
Bartek <tech@euro-net.pl>
2015-06-26 16:22:45 +0200

    poprawki karta klienta i pin na fakturze

--- modules/invoice_tcpdf.inc.php
+++ modules/invoice_tcpdf.inc.php
@@ -407,7 +407,7 @@ function invoice_buyer() {
 	$pdf->writeHTMLCell(80, '', 125, 50, $postbox, 0, 1, 0, true, 'L');
 
 	$pin = '<b>' . trans('Customer ID: $a', sprintf('%04d', $invoice['customerid'])) . '</b><br>';
-	$pin .= '<b>PIN: ' . sprintf('%04d', $invoice['customerpin']) . '</b><br>';
+	//$pin .= '<b>PIN: ' . sprintf('%04d', $invoice['customerpin']) . '</b><br>';
 
 	$pdf->SetFont('arial', 'B', 8);
 	$pdf->writeHTMLCell('', '', 125, $oldy + round(($y - $oldy) / 2), $pin, 0, 1, 0, true, 'L');
